48d88e7e28579973b2746cecb353a9412e540618
nbos <35620093+nbos@users.noreply.github.com>
2019-07-11 18:55:19 +0200

    Fix under-escaping of backslashes in strings

--- sempre/src/edu/stanford/nlp/sempre/interactive/lassie/LassieUtils.java
+++ sempre/src/edu/stanford/nlp/sempre/interactive/lassie/LassieUtils.java
@@ -25,20 +25,31 @@ public class LassieUtils{
 
     // Rudimentary translation of a json object into an SML record
     public static String json2sml(String string) {
-	// dependent on knowing the fields
-	List<String> fields = Lists.newArrayList("candidates", "score", "prob", "anchored", "formula",
-						 "stats", "size", "status", "lines");
+	// dependent on knowing the fields in advance
+	String[] fields = {"candidates", "score", "prob", "anchored", "formula",
+			   "stats", "size", "status", "lines"};
 	// unquote fields; subsitute `:` for `=`
 	for (String field : fields) {
 	    string = string.replace("\"" + field + "\":", field + "= ");
 	}
 
-	string =
-	    string
+	string = string
 	    .replace("\"type\":", "cmd= ") // avoid reserved keywords of SML
 	    .replace("\"NaN\"", "~1.0") // force types of fields
 	    .replaceAll("\"value\":\"(.*?)\"","value= \"$1\",tactic= $1"); // cast the value as a tactic
 	
+	// escape backslashes in strings
+	// (we could do more fancy escaping, but quotes are already converted earlier by
+	//  sempre and other characters requiring escaping are not expected to appear here)
+	String[] substrings = string.split("(?<!\\\\)(?:\\\\\\\\)*\"");
+	assert (substrings.length % 2 == 1); // we assume the Json had matching quotes and does not begin or end with them
+	string = substrings[0];
+	for (int i = 1; i < substrings.length; i++) {
+	    if (i % 2 == 1) // every second substring is data of type string
+		substrings[i] = substrings[i].replace("\\","\\\\");
+	    string = string + "\"" + substrings[i];
+	}
+		    
 	return string;
     }
 }
