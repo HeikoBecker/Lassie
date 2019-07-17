06bbbb4e7eb0d8ccfd457572cdef03d06dfcf960
nbos <35620093+nbos@users.noreply.github.com>
2019-07-17 17:28:56 +0200

    Fix bugs related to importing and filtering through feature-sets

--- sempre/src/edu/stanford/nlp/sempre/interactive/lassie/TacticWorld.java
+++ sempre/src/edu/stanford/nlp/sempre/interactive/lassie/TacticWorld.java
@@ -32,12 +32,11 @@ public class TacticWorld extends World {
 	@Option(gloss = "Path to database file, contains components and their features")
 	public String dbPath = null;
     }
-    
     public static Options opts = new Options();
     
     public String constructedTactic;
-    public Map<String,Map<String,Set<String>>> entities; // component (name) -> feature -> values
-    public Map<String,Set<String>> features; // feature -> components (name)
+    public Map<String,Map<String,Set<String>>> entities; // component (its name) -> attribute -> feature
+    public Map<String,Set<String>> features; // feature -> components (its name)
     
     @SuppressWarnings("unchecked")
     public TacticWorld() {
@@ -49,39 +48,44 @@ public class TacticWorld extends World {
 	readDB();
     }
     
-    private void insert(String component, String feature, Set<String> values) {
-	for (String val : values) insert(component, feature, val);
+    private void insert(String component, String attribute, Set<String> features) {
+	for (String feat : features) {
+	    LogInfo.logs("inserting: %s, %s, %s", component, attribute, feat);
+	    insert(component, attribute, feat);
+	}
     }
-    private void insert(String component, String feature, String value) {
+    private void insert(String component, String attribute, String feature) {
 	// From components to their features
-	entities.putIfAbsent(component, new HashMap<String,Set<String>>());
-	Map<String,Set<String>> features = entities.get(component);
-	features.putIfAbsent(feature, new HashSet<String>());
-	Set<String> values = features.get(feature);
-	values.add(value);
-	features.put(feature, values);
-	entities.put(component, features);
-	// From features to components which satisfy them
-	features.putIfAbsent(feature, new HashSet<String>());
-	Set<String> components = features.get(feature);
+	this.entities.putIfAbsent(component, new HashMap<String,Set<String>>());
+	Map<String,Set<String>> attributes = entities.get(component);
+	attributes.putIfAbsent(attribute, new HashSet<String>());
+	Set<String> features = attributes.get(attribute);
+	features.add(feature);
+	attributes.put(attribute, features);
+	entities.put(component, attributes);
+	// From features to components which possess them
+	this.features.putIfAbsent(feature, new HashSet<String>());
+	Set<String> components = this.features.get(feature);
 	components.add(component);
-	features.put(feature, components);
+	LogInfo.logs("  adding: %s :: %s", feature, component);
+	this.features.put(feature, components);
     }
     
     private void readDB() {
 	LogInfo.begin_track("TacticWorld.readEntities: %s", opts.dbPath);
 	// Load up from database
 	for (String line : IOUtils.readLinesHard(opts.dbPath)) {
+	    LogInfo.logs("Processing line: %s", line);
 	    if (line.startsWith("#")) continue; // Skip comment lines
-	    String[] tokens = line.split("\\s");
+	    String[] tokens = line.split("\\s+");
 	    // We expect triplets, e.g. "POW_2 feature.name feature.name.power"
 	    if (tokens.length >= 3) {  
 		String component = tokens[0];
-		String feature = tokens[1];
-		Set<String> values = new HashSet();
+		String attribute = tokens[1];
+		Set<String> features = new HashSet();
 		for (int i = 2; i < tokens.length; i++)
-		    values.add(tokens[i]);
-		insert(component, feature, values);
+		    features.add(tokens[i]);
+		insert(component, attribute, features);
 	    } else {
 		throw new RuntimeException("Unhandled: " + line);
 	    }
@@ -103,7 +107,7 @@ public class TacticWorld extends World {
 	return tac1 + " >- " + tac2;
     }
     public String cons(String hd, String tl) {
-	return hd + ", " + tl;
+	return hd + "," + tl;
     }
     public String list(String seq) {
 	return "[" + seq + "]";
@@ -128,55 +132,56 @@ public class TacticWorld extends World {
 	return String.join(".",Arrays.copyOfRange(addr, 0, addr.length - 2));
     }
     public Set<String> fromFeature(String f) {
-	if (f == "top") return entities.keySet();
-	else if (f == "bot") return new HashSet<String>();
+	if (f.equals("top")) return entities.keySet();
+	else if (f.equals("bot")) return new HashSet<String>();
         else if (features.containsKey(f)) return features.get(f);
 	else throw new RuntimeException("Feature not recognized: " + f);
     }
     
     // Set operations
-    public Set<String> filter(Set<String> s1, Set<String> s2) {
+    public Set<String> intersect(Set<String> s1, Set<String> s2) {
 	return s1.stream().filter(i -> s2.contains(i)).collect(Collectors.toSet());
     }
-    public Set<String> filter(Set<String> s1, String s2) {
-	if (s2 == "top") return s1;
-	else if (s2 == "bot") return new HashSet<String>();
-	else return s1.stream().filter(i -> features.get(s2).contains(i)).collect(Collectors.toSet());
-    }
-    public Set<String> filter(String s1, Set<String> s2) {
-	return filter(s2,s1);
-    }
-    public Set<String> filter(String s1, String s2) {
-	if (s1 == "bot" || s2 == "bot") return new HashSet<String>();
-	else if (s1 == "top") return fromFeature(s2);
-	else if (s2 == "top") return fromFeature(s1);
-	else return filter(fromFeature(s1),fromFeature(s2));
+    // public Set<String> filter(Set<String> s1, String s2) {
+    // 	if (s2.equals("top")) return s1;
+    // 	else if (s2.equals("bot")) return new HashSet<String>();
+    // 	else return s1.stream().filter(i -> features.get(s2).contains(i)).collect(Collectors.toSet());
+    // }
+    // public Set<String> filter(String s1, Set<String> s2) {
+    // 	return filter(s2,s1);
+    // }
+    // public Set<String> filter(String s1, String s2) {
+    // 	if (s1.equals("bot") || s2.equals("bot")) return new HashSet<String>();
+    // 	else if (s1.equals("top")) return fromFeature(s2);
+    // 	else if (s2.equals("top")) return fromFeature(s1);
+    // 	else return filter(fromFeature(s1),fromFeature(s2));
+    // }
+    public String set2string(Set<String> s) {
+	return String.join(",", s);
     }
 	
-
     // Result
     public void tacReturn(String str) { this.constructedTactic = str; }
-    
-    @Override
-    public Set<Item> has(String rel, Set<Object> values) {
-	// LogInfo.log(values);
-	return this.allItems.stream().filter(i -> values.contains(i.get(rel))).collect(Collectors.toSet());
-    }
-
-    @Override
-    public Set<Object> get(String rel, Set<Item> subset) {
-	return subset.stream().map(i -> i.get(rel)).collect(Collectors.toSet());
-    }
-    
-    @Override
-    public void update(String rel, Object value, Set<Item> selected) {}
-    
-    @Override
-    public void merge() {}
 
     // Called from DALExecutor after evaluation; is the return value of executor
     public String toJSON() {
 	return this.constructedTactic;
     }
-    
+
+    ///////////////////////////////
+    // UNUSED, required by interface
+    @Override
+    public Set<Item> has(String rel, Set<Object> values) {
+	// LogInfo.log(values);
+	return this.allItems.stream().filter(i -> values.contains(i.get(rel))).collect(Collectors.toSet());
+    }
+    @Override
+    public Set<Object> get(String rel, Set<Item> subset) {
+	return subset.stream().map(i -> i.get(rel)).collect(Collectors.toSet());
+    }
+    @Override
+    public void update(String rel, Object value, Set<Item> selected) {}
+    @Override
+    public void merge() {}
+    ///////////////////////////////
 }
